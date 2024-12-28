f790a231358625685dfc63b66ebbbdd89cc62434
jjf2009 <jaredfurtadowork@gmail.com>
2024-12-28 07:47:30 +0530

    Fixed Typos

--- script.js
+++ script.js
@@ -1,71 +1,67 @@
-
 const projects = [
     {
         title: "Steve Jobs Tribute",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image:"./Projects-images/01.png",
+        description: "A tribute page dedicated to Steve Jobs, showcasing responsive design and modern web principles.",
+        image: "./images/01.png",
         web: "https://steve-jobs-tribute.vercel.app/",
         git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
     },
     {
         title: "Pokedex",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/02.png",
+        description: "An interactive Pokedex application built to showcase Pokemon data with responsive design.",
+        image: "./images/02.png",
         web: "https://pokedex-rho-lake.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/PokemonSearchApp"
     },
     {
         title: "Connect 4",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/03.png",
+        description: "A fun Connect 4 game implemented with engaging UI and responsive design.",
+        image: "./images/03.png",
         web: "https://connet-4.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/Connet-4"
     },
     {
         title: "Todo App",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/04.png",
+        description: "A simple and efficient Todo application to manage tasks with responsive design.",
+        image: "./images/04.png",
         web: "https://basic-todo-one.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/BasicTodo"
     },
     {
-        title: "Company Registeration Potal",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/05.png",
+        title: "Company Registration Portal",
+        description: "A portal for registering companies with a user-friendly interface and responsive design.",
+        image: "./images/05.png",
         web: "https://companydirectoryportal.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: " https://github.com/jjf2009/CompanyDirectoryPortal.git"
     },
     {
         title: "Country Code Finder",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/06.png",
+        description: "A tool to search and find country codes, built with a responsive interface.",
+        image: "./images/06.png",
         web: "https://country-search-hackathon.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/Search-Bar-Hackathon"
     },
     {
         title: "Nike Website Clone",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/07.png",
+        description: "A clone of the Nike website with responsive design and modern web practices.",
+        image: "./images/07.png",
         web: "https://nike-landing-lac.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/Nike_New"
     },
     {
         title: "Palindrome Checker",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/08.png",
+        description: "An application to check if a word or phrase is a palindrome, featuring responsive design.",
+        image: "./images/08.png",
         web: "https://palindrome-checker-chi.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
+        git: "https://github.com/jjf2009/Palindrome_Checker"
     },
     {
         title: "Roman Number Converter",
-        description: "A responsive tribute page built with HTML5 and CSS3, showcasing modern web design principles.",
-        image: "./Projects-images/09.png",
+        description: "A tool to convert numbers to Roman numerals, built with a responsive layout.",
+        image: "./images/09.png",
         web: "https://roman-number-converter-delta.vercel.app/",
-        git: "https://github.com/jjf2009/Steve_Jobs_Tribute"
-    },
-    
-    
-    
+        git: "https://github.com/jjf2009/Roman_Number_Converter-"
+    }
 ];
 
 
